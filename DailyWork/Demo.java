b583ac5b86112b840355da1e944f676ac04727a9
Sham <shamlomate2114@gmail.com>
2024-09-04 17:39:27 +0530

    Add files via upload

---
+++ DailyWork/Demo.java
@@ -0,0 +1,126 @@
+/*Q1*/
+/*public class Demo{
+	public static void main(String[] args) {
+		System.out.println("     *");
+		System.out.println("    * *");
+		System.out.println("   *   *");
+		System.out.println("  *     *");
+		System.out.println(" *       *");
+		System.out.println("***********");
+	}
+}
+*/
+/*Q2
+public class Demo{
+		public static void main(String[] args) {
+			System.out.println("***********");
+			System.out.println(" *       *");
+			System.out.println("  *     *");
+			System.out.println("   *   *");
+			System.out.println("    * *");
+			System.out.println("     *");
+		}
+}
+
+Q3
+public class Demo{
+	public static void main(String[] args) {
+		System.out.println("     A");
+		System.out.println("    BCD");
+		System.out.println("   EFGHI");
+		System.out.println("  JKLMNOP");
+		System.out.println(" QRSTUVWXYZ");
+	}
+}
+
+Q4
+public class Demo{
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("1 2");
+		System.out.println("1   3");
+		System.out.println("1     4");
+		System.out.println("1 2 3 4 5");
+	}
+}
+Q5
+public class Demo{
+	public static void main(String[] args) {
+		System.out.println("       1");
+		System.out.println("     1 2 1");
+		System.out.println("    1 2 3 2 1");
+		System.out.println("   1 2 3 4 3 2 1");
+		System.out.println(" 1 2 3 4 5 4 3 2 1");
+		
+	}
+}
+Q6
+public class Demo{
+	public static void main(String[] args) {
+		System.out.println("     1");
+		System.out.println("    121");
+		System.out.println("   12321");
+		System.out.println("  1234321");
+		System.out.println("   12321");
+		System.out.println("    121");
+		System.out.println("     1");
+	}
+}
+Q7
+public class Demo{
+	public static void main(String[] args) {
+		System.out.println("1                 1");
+		System.out.println("1 2             2 1");
+		System.out.println("1 2 3         3 2 1");
+		System.out.println("1 2 3 4     4 3 2 1");
+		System.out.println("1 2 3 4 5 5 4 3 2 1");
+	}
+}
+
+Q8
+public class Demo{
+	public static void main(String[] args) {
+		System.out.println("0");
+		System.out.println("505");
+		System.out.println("45054");
+		System.out.println("3450543");
+		System.out.println("234505432");
+		System.out.println("12345054321");
+	}
+}
+
+Q9
+
+public class Demo{
+	public static void main(String[] args) {
+		System.out.println("        1 x 8 + 1 = 9 ");
+		System.out.println("       12 x 8 + 2 = 98 ");
+		System.out.println("      123 x 8 + 3 = 987 ");
+		System.out.println("     1234 x 8 + 4 = 9876 ");
+		System.out.println("    12345 x 8 + 5 = 98765  ");
+		System.out.println("   123456 x 8 + 6 = 987654  ");
+		System.out.println("  1234567 x 8 + 7 = 9876543 ");
+		System.out.println(" 12345678 x 8 + 8 = 98765432");
+		System.out.println("123456789 x 8 + 9 = 987654321");
+
+	}
+}
+Q10
+public class Demo{
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2 4");
+		System.out.println("3 6 9");
+		System.out.println("4 8 12 16");
+		System.out.println("5 10 15");
+		System.out.println("6 12");
+		System.out.println("7");
+	}
+}
+
+
+
+
+
+
+*/
